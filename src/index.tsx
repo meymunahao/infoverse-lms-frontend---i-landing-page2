0fc941428d868f56fa04568df493b66763d6c327
meymunahao <115807623+meymunahao@users.noreply.github.com>
2025-09-21 22:44:40 +0100

    Add routing and admin dashboard page

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AdminDashboardPage from "./app/admin/dashboard/page";
 import { ILandingPage } from "./screens/ILandingPage/ILandingPage";
 import "../tailwind.css";
 
 createRoot(document.getElementById("app") as HTMLElement).render(
   <StrictMode>
-    <ILandingPage />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ILandingPage />} />
+        <Route path="/admin/dashboard" element={<AdminDashboardPage />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 );
